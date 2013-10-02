8c364dd27b43e840015671de1a95ca7eb95d6314
arno <arno@uberpc>
2013-10-02 16:38:15 +0200

    First commit

---
+++ Oefeningen/src/Oefeningen.cpp
@@ -0,0 +1,16 @@
+//============================================================================
+// Name        : Oefeningen.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	int x = 5;
+	cout << "!!!Hello World!!!" << x << endl; // prints !!!Hello World!!!
+	return 0;
+}
