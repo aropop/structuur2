a30c3e575967544aa301dbc2d01595940b2092cd
arno <arno@uberpc>
2013-10-02 17:57:09 +0200

    les3

--- Oefeningen/src/les1.cpp
+++ Oefeningen/src/les1.cpp
@@ -10,10 +10,13 @@
 using namespace std;
 
 int main() {
-	int x,y;
-	cin >> x;
-	cin >> y;
-	cout << x << endl;
-	cout << y << endl;
+	while (true) {
+		double x, y;
+		cin >> x;
+		cin >> y;
+
+		cout << x / y << endl;
+
+	}
 	return 0;
 }
