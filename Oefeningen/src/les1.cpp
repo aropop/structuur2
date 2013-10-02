17e7f7d3aa24401fc7a0b5050e0db80c307290d1
arno <arno@uberpc>
2013-10-02 17:28:03 +0200

    rename oefeningen to les 1

---
+++ Oefeningen/src/les1.cpp
@@ -0,0 +1,16 @@
+//============================================================================
+// Name        : Oefeningen.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "1" << endl;
+	cout << "2" << endl;
+	return 0;
+}
