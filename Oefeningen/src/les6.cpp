f9c30db7d95db8d7c554d005176f31911c42dc04
Arno De Witte <Arno.De.Witte@vub.ac.be>
2013-11-08 14:08:51 +0100

    les 6 deel 2

--- Oefeningen/src/les6.cpp
+++ Oefeningen/src/les6.cpp
@@ -5,25 +5,27 @@
  *      Author: arno
  */
 #include <iostream>
+#include <sys/stat.h>
+#include <sys/types.h>
 /* oefening 1 */
-void swap (int* a, int* b){
+void swap(int* a, int* b) {
 	int temp = *a;
 	*a = *b;
 	*b = temp;
 }
 /* oefening 2 */
-void read_array(){
+void read_array() {
 	int n;
 	std::cin >> n;
 	int size(n);
 	int * array = new int[n]; //pointer naar het eerste element van de array
 	//de vierkante haken gaan gewoon telkens een int verder in het geheugen
-	while (n > 0){
+	while (n > 0) {
 		std::cin >> array[n];
 		--n;
 	}
 	n = size;
-	while (n > 0){
+	while (n > 0) {
 		std::cout << array[n] << std::endl;
 		--n;
 	}
@@ -32,15 +34,16 @@ void read_array(){
 /* Oefening 3 */
 class Ulist {
 public:
-	Ulist(int size): size_(size){
+	Ulist(int size) :
+			size_(size) {
 		first = new Node(size);
 	}
 
-	void add(int element){
+	void add(int element) {
 		Node * current;
 		current = first;
-		while(current->is_full()){
-			if(current->next == NULL){
+		while (current->is_full()) {
+			if (current->next == NULL) {
 				current->next = new Node(size_);
 
 			}
@@ -49,19 +52,66 @@ public:
 		current->data[current->free++] = element;
 	}
 
-	void print(){
+	bool remove(int element) {
+		Node * current(first);
+		bool found(false);
+		while (current->next != NULL) {
+			int index(0);
+			while (index < size_) {
+				if (current->data[index] == element) {
+					current->remove(index);
+					found = true;
+					break;
+				}
+				++index;
+			}
+			if (found) {
+				break;
+			}
+			current = current->next;
+		}
+		return found;
+	}
+
+	unsigned int size() const {
+		const Node * current(first);
+		int sizer(0);
+		while (current != NULL) {
+			sizer += current->free;
+			current = current->next;
+		}
+		return sizer;
+	}
+
+	int get(int indx) {
+		Node * current(first);
+		int counter(0);
+		while (counter < indx) {
+			counter += current->free;
+			if (counter >= indx) {
+				return current->data[indx - (counter - current->free)];
+			} else if (current->next == NULL) {
+				throw 123;
+			}
+			current = current->next;
+		}
+		return 0;
+	}
+
+	void print() const {
 		Node * current;
 		current = first;
-		while(current->is_full()){
-			for(int i =0; i < current->free; i++){
+		while (current->next != NULL) {
+			for (int i = 0; i < current->free; i++) {
 				std::cout << current->data[i] << " ";
 			}
 			std::cout << std::endl;
 			current = current->next;
 		}
-		for(int i =0; i < current->free; i++){
-				std::cout << current->data[i] << " ";
+		for (int i = 0; i < current->free; i++) {
+			std::cout << current->data[i] << " ";
 		}
+		std::cout << std::endl;
 	}
 
 private:
@@ -71,27 +121,64 @@ private:
 		int * data;
 		int free;
 		Node * next;
-		Node(int size_in){
+		Node(int size_in) {
 			data = new int[size_in];
 			size = size_in;
 			free = 0;
 			next = NULL;
 		}
-		bool is_full(){
+		void remove(int indx) {
+			int to_swap(free - indx - 1);
+			while (to_swap > 0) {
+				data[indx + to_swap - 1] = data[indx + to_swap];
+				to_swap = to_swap - 1;
+			}
+			free = free - 1;
+		}
+		bool is_full() {
 			return (free == size);
 		}
 	};
 	Node* first;
 };
 
+/*oef4*/
+class statInterface {
+public:
+	statInterface(const char * filename) {
+		stat(filename, buffer_);
+	}
 
-int main(int argc, char *args[]){
+	int getMode() {
+		return buffer_->st_mode;
+	}
+
+	struct stat * operator->() {
+		return buffer_;
+	}
+private:
+	struct stat * buffer_;
+};
+
+int main(int argc, char *args[]) {
 	//read_array();
-	Ulist l = Ulist(3);
-	l.add(5);
-	l.print();
+//	Ulist l = Ulist(3);
+//	l.add(5);
+//	l.add(6);
+//	l.add(3);
+//	l.add(1);
+//	l.print();
+//	std::cout << l.size() << std::endl;
+//	l.remove(6);
+//	l.print();
+//	try{
+//		l.get(100);
+//		l.get(3);
+//	}catch(int e){
+//		std::cout << "not in ul" << std::endl;
+//	}
+	statInterface si ("text.txt");
+	std::cout << si->st_mode << std::endl;
 	return 0;
 }
-
-
 
